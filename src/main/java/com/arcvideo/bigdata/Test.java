232c9ef3fd555553d6a395a0053c4c490ae83261
Sun Tao[孙涛] <st@arcvideo.com>
2017-03-10 17:49:39 +0800

    first commit

---
+++ src/main/java/com/arcvideo/bigdata/Test.java
@@ -0,0 +1,11 @@
+package com.arcvideo.bigdata;
+
+/**
+ * Created by st@arcvideo.com on 2017/3/10.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,ResourceManager!");
+    }
+}
